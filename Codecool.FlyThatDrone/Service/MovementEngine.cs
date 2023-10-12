b627644c58bc06b85f3a3584aaf4d8ba575fac28
davidszoki <davidszokoloczi@gmail.com>
2023-10-12 14:04:29 +0200

    feat: Implement movement ^--^  ^------------^ |     | |     +-> Impementing the basic ground movement.

---
+++ Codecool.FlyThatDrone/Service/MovementEngine.cs
@@ -0,0 +1,27 @@
+using Codecool.FlyThatDrone.Model;
+
+namespace Codecool.FlyThatDrone.Service;
+
+public class MovementEngine : IMovementEngine
+{
+    public bool Move(Drone drone, string direction)
+    {
+        switch (direction)
+        {
+            case "w":
+                drone.Position = new Position(drone.Position.X, drone.Position.Y + 1, drone.Position.Z);
+                return true;
+            case "s":
+                drone.Position = new Position(drone.Position.X, drone.Position.Y - 1, drone.Position.Z);
+                return true;
+            case "a":
+                drone.Position = new Position(drone.Position.X - 1, drone.Position.Y, drone.Position.Z);
+                return true;
+            case "d":
+                drone.Position = new Position(drone.Position.X + 1, drone.Position.Y, drone.Position.Z);
+                return true;
+           default:
+               return false;
+        }
+    }
+}
